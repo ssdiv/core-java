d271fcb17b6124a272783e91e44bdeeb5c4dcb62
ssdiv <divyas.xworkz@gmail.com>
2023-11-03 00:56:35 +0530

    courseStarter

---
+++ CourseStarter.java
@@ -0,0 +1,67 @@
+class CourseStarter{
+   public static void main(String[] courses)
+   {
+	   System.out.println("Starting main in CourseStarter");
+	   
+	   Course course=new Course("xworkz","java",30,10,"btm",5,4.5);
+	   course.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Course course1=new Course("Kodnest","java",30,10,"btm",5,4.7);
+	   course1.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Course course2=new Course("Bright","python",30,10,"btm",5,3.0);
+	   course2.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Course course3=new Course("pankaj","java",30,10,"btm",5,4.5);
+	   course3.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Course course4=new Course("karunadu","Ml",30,10,"btm",5,4.5);
+	   course4.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Course course5=new Course("ceso","java",30,10,"btm",5,4.5);
+	   course5.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Course course6=new Course("delight","sql",30,10,"btm",5,3.0);
+	   course6.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Course course7=new Course("vishnu","java",30,10,"kormangla",5,4.5);
+	   course7.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Course course8=new Course("kavyash","python",30,10,"JP nagar",5,4.0);
+	   course8.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Course course9=new Course("Kushvik","java",30,10,"mg road",5,3.0);
+	   course9.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Course course10=new Course("divya","java",30,10,"btm",5,4.5);
+	   course10.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Course course11=new Course("Rsham","java",30,10,"btm",5,3.6);
+	   course11.displayInfo();
+	   
+	   System.out.println("ending main in CourseStarter");
+	   
+   }
+}
