8f3dbac5988859c6281b6145dc072fe54fdc7421
ssdiv <divyas.xworkz@gmail.com>
2023-11-03 00:58:23 +0530

    PlacementStarter

---
+++ PlacementStarter.java
@@ -0,0 +1,67 @@
+class PlacementStarter{
+
+   public static void main(String[] programstarter)
+   {
+	   System.out.println("Starting main in PlacementStarter");
+	   
+	   Placement placement=new Placement("GMIT",50,5,"blore",false,true,"offline","bajaj",10,"software trainee",10,3.5);
+	   placement.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Placement placement1=new Placement("ubdt",50,5,"blore",false,true,"offline","IBM",10,"software trainee",10,3.0);
+	   placement1.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Placement placement2=new Placement("reva",50,5,"blore",false,true,"offline","apple",10,"software trainee",10,3.1);
+	   placement2.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Placement placement3=new Placement("acharya",50,5,"blore",false,true,"offline","microsoft",10,"software trainee",10,3.2);
+	   placement3.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Placement placement4=new Placement("rnsit",50,5,"blore",false,true,"offline","cisco",10,"software trainee",10,3.3);
+	   placement4.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Placement placement5=new Placement("dayanad sagar",50,5,"blore",false,true,"offline","dell",10,"software trainee",10,3.4);
+	   placement5.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Placement placement6=new Placement("presidency",50,5,"blore",false,true,"offline","asus",10,"software trainee",10,3.5);
+	   placement6.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Placement placement7=new Placement("christ",50,5,"blore",false,true,"offline","Accenture",10,"software trainee",10,4.1);
+	   placement7.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Placement placement8=new Placement("peset",50,5,"blore",false,true,"offline","congnizent",10,"software trainee",10,3.5);
+	   placement8.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Placement placement9=new Placement("spc",50,5,"blore",false,true,"offline","honeywell",10,"software trainee",10,3.6);
+	   placement9.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Placement placement10=new Placement("biet",50,5,"blore",false,true,"offline","infosys",10,"software trainee",10,3.7);
+	   placement10.displayInfo();
+	   
+	   System.out.println("----------------------");
+	   
+	   Placement placement11=new Placement("jain",50,5,"blore",false,true,"offline","tcs",10,"software trainee",10,5.0);
+	   placement11.displayInfo();
+	   
+	   System.out.println("ending main in PlacementStarter");
+   }
+}
