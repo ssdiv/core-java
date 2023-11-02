3f2e4db9cbb7f37cb7e75fbeecbc61b2c8af061b
ssdiv <divyas.xworkz@gmail.com>
2023-11-03 00:55:27 +0530

    MonthStarter

---
+++ MonthStarter.java
@@ -0,0 +1,68 @@
+class MonthStarter
+{
+	public static void main(String[] monthstarters)
+	{
+		
+		System.out.println("Starting main in MonthStarter");
+		
+		Month month=new Month("january",31,4,7);
+		month.displayInfo();
+		
+		System.out.println("----------------------");
+		
+		Month month1=new Month("february",28,4,5);
+		month1.displayInfo();
+		
+		System.out.println("----------------------");
+		
+		Month month2=new Month("march",31,4,5);
+		month2.displayInfo();
+		
+		System.out.println("----------------------");
+		
+		Month month3=new Month("april",30,4,5);
+		month3.displayInfo();
+		
+		System.out.println("----------------------");
+		
+		Month month4=new Month("may",31,4,5);
+		month4.displayInfo();
+		
+		System.out.println("----------------------");
+		
+		Month month5=new Month("june",30,4,6);
+		month5.displayInfo();
+		
+		System.out.println("----------------------");
+		
+		Month month6=new Month("july",31,4,5);
+		month6.displayInfo();
+		
+		System.out.println("----------------------");
+		
+		Month month7=new Month("august",30,4,7);
+		month7.displayInfo();
+		
+		System.out.println("----------------------");
+		
+		Month month8=new Month("spetember",31,4,7);
+		month8.displayInfo();
+		
+		System.out.println("----------------------");
+		
+		Month month9=new Month("october",31,4,6);
+		month9.displayInfo();
+		
+		System.out.println("----------------------");
+		
+		Month month10=new Month("november",31,4,6);
+		month10.displayInfo();
+		
+		System.out.println("----------------------");
+		
+		Month month11=new Month("december",31,4,6);
+		month11.displayInfo();
+		
+		System.out.println("Ending main in FreeStarter");
+	}
+}
