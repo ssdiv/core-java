95323b5f3094d6103e15a436a1e674e6fdff0e5d
ssdiv <divyas.xworkz@gmail.com>
2023-11-08 11:39:13 +0530

    GameStarter

---
+++ GameStarter.java
@@ -0,0 +1,30 @@
+class GameStarter{
+	
+	public static void main(String[] apply){
+		
+		System.out.println("Starting main in Starter");
+		System.out.println("Starting Tent");
+		Color color=Color.Purple;
+		Tent tent=new Tent();
+		Agent agent=new Agent();
+		tent.showInfo(23,color,35,50);
+		agent.openTent(tent);
+		agent.closeTent(tent);
+		agent.showInfo();
+		System.out.println("Ending Tent");
+		
+		System.out.println("Starting Game");
+		TotalPlayers totalPlayers=TotalPlayers.Fifty;
+		Game game=new Game(20,"Football",totalPlayers,"Outdoor");
+		Player player=new Player("Richard",game);
+		
+		game.showInfo();
+		player.showInfo();
+		player.playGame();
+		player.pauseGame();
+		player.endGame();
+		
+		System.out.println("Starting main in Starter");
+	}
+
+}
