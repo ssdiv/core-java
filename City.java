74fe4adf17dbaf66dab44802ae5f8c4e6facc109
ssdiv <divyas.xworkz@gmail.com>
2023-11-15 22:02:37 +0530

    City

---
+++ City.java
@@ -0,0 +1,22 @@
+class City{
+		
+		String name="Davanagere";
+		String statename="Karnataka";
+			
+		City()
+		{
+			
+		}		
+		void show()
+		{
+			System.out.println("show information of city");
+		    System.out.println("name :"+this.name);
+			System.out.println("name :"+this.statename);
+			System.out.println("end information of city");
+			
+			
+		}
+		
+
+
+}
